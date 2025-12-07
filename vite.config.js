ac0f6ab8122f435955f419890f34cdd63d48aad2
Kevin Code <kevindodiya75@gmail.com>
2025-12-07 23:21:43 +0000

    feta: add priority to the search function

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  plugins: [svelte()],
+
+  build: {
+    outDir: "static/dist",
+    emptyOutDir: true,
+
+    rollupOptions: {
+      input: {
+        svelte: path.resolve(__dirname, "assets/js/svelte/main.js"),
+      },
+      output: {
+        entryFileNames: "js/[name].js",
+        chunkFileNames: "js/[name]-[hash].js",
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name.endsWith(".css")) {
+            return "css/[name][extname]";
+          }
+          return "assets/[name][extname]";
+        },
+      },
+    },
+    manifest: true,
+  },
+});
